3bb20ed42c6765157aa077bc2bac746c6f1bc8d9
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 <anghildiyall17@gmail.com>
2026-05-16 13:48:57 -0600

    first commit

---
+++ src/main/java/com/ag/SbAppApplication.java
@@ -0,0 +1,13 @@
+package com.ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAppApplication.class, args);
+	}
+
+}
